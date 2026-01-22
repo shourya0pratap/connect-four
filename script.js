3bab50392f0641315d12d645c275e0ec55508d76
sp_3141 <07shourya.pratap@gmail.com>
2026-01-22 19:26:25 +0530

    Added a functional reset button and animations for slots falling

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ let gameRunning = true;
 const boardState = new Array(42).fill(null);
 
 const grid = document.getElementById("game-grid");
+const btn = document.getElementById("reset-btn");
+
 for (let i = 0; i < 42; i++) {
   const cell = document.createElement("div");
   cell.classList.add("slot");
@@ -31,8 +33,47 @@ grid.addEventListener("click", (e) => {
       boardState[ind] = playerTeams[currentPlayer];
       const targetCell = document.querySelector(`.slot[data-index='${ind}']`);
       targetCell.style.backgroundColor = playerColors[currentPlayer];
+      targetCell.classList.add("falling");
       currentPlayer = (currentPlayer + 1) % 2;
       break;
     }
   }
+});
+
+function resetGame() {
+  boardState.fill(null);
+  const allSlots = document.querySelectorAll(".slot");
+  allSlots.forEach((slot) => {
+    slot.style.backgroundColor = ""; // Remove color
+    slot.classList.remove("falling"); // Remove class so it can play again later
+  });
+}
+
+btn.addEventListener("mouseenter", () => {
+  btn.innerHTML = "Reset??";
+  btn.style.cursor = "pointer";
+  btn.style.transform = "scale(1.1) rotateZ(-15deg)";
+  btn.style.boxShadow = "10px 10px 10px black";
+});
+
+btn.addEventListener("mouseleave", () => {
+  btn.innerHTML = "Reset?";
+  btn.style.transform = "scale(1)";
+  btn.style.boxShadow = "none";
+});
+
+btn.addEventListener("mousedown", () => {
+  btn.innerHTML = "RESET!";
+  btn.style.transform = "scale(0.95)";
+  btn.style.boxShadow = "none";
+  btn.style.backgroundColor = "black";
+  btn.style.color = "white";
+});
+
+btn.addEventListener("mouseup", () => {
+  btn.innerHTML = "Reset?";
+  btn.style.transform = "scale(1)";
+  btn.style.backgroundColor = "white";
+  btn.style.color = "black";
+  resetGame();
 });
